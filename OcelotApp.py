d91942a9ddf971b0bd0024ec6822f66de2f5ac72
daleparkerwinston <daleparkerwinston@gmail.com>
2016-03-01 19:30:26 -0500

    New

--- OcelotApp.py
+++ OcelotApp.py
@@ -3,29 +3,31 @@ from flask import render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-	return render_template("index.html")
+    return render_template("index.html")
+
 
 @app.route('/<math>/<int:num1>/<int:num2>')
 @app.route('/<math>/<float:num1>/<float:num2>')
 @app.route('/<math>/<int:num1>/<float:num2>')
 @app.route('/<math>/<float:num1>/<int:num2>')
 def math(num1, num2, math):
-	if math == "add":
-		answer = num1+num2
-		symbol = '+'
-	if math == "sub":
-		answer = num1-num2
-		symbol = '-'
-	if math == "mul":
-		answer = num1*num2
-		symbol = '*'
-	if math == "div":
-		answer = num1/num2
-		symbol = '/'
-	context = {'num1': num1, 'num2': num2, 'answer': answer, 'symbol': symbol}
-	return render_template("math.html", **context)
+    if math == "add":
+        answer = num1 + num2
+        symbol = '+'
+    if math == "sub":
+        answer = num1 - num2
+        symbol = '-'
+    if math == "mul":
+        answer = num1 * num2
+        symbol = '*'
+    if math == "div":
+        answer = num1 / num2
+        symbol = '/'
+    context = {'num1': num1, 'num2': num2, 'answer': answer, 'symbol': symbol}
+    return render_template("math.html", **context)
 
 
 if __name__ == "__main__":
