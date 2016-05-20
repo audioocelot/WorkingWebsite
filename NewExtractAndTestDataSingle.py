f403da2840c2b7b46b05311dcc90d6d359754543
simplymarcello <simplymarcello@gmail.com>
2016-05-20 15:56:55 -0400

    fix broken min/max/ptp files

--- NewExtractAndTestDataSingle.py
+++ NewExtractAndTestDataSingle.py
@@ -36,26 +36,26 @@ rd = os.system(
 
 inpt = GetFeatures("/home/ubuntu/OcelotApp/Temp.csv")
 
-CHANGE_PTP = False
-for x in range(len(inpt)):
-    if inpt[x] < MIN[x]:
-        CHANGE_PTP = True
-        MIN[x] = inpt[x]
-    if inpt[x] > MAX[x]:
-        CHANGE_PTP = True
-        MAX[x] = inpt[x]
+# CHANGE_PTP = False
+# for x in range(len(inpt)):
+#    if inpt[x] < MIN[x]:
+#        CHANGE_PTP = True
+#        MIN[x] = inpt[x]
+#    if inpt[x] > MAX[x]:
+#        CHANGE_PTP = True
+#        MAX[x] = inpt[x]
 
-if CHANGE_PTP:
-    temp = [MAX[x] - MIN[x] for x in range(len(PTP))]
-    PTP = np.array(temp)
-    np.save("/home/ubuntu/OcelotApp/MIN.npy", MIN)
-    np.save("/home/ubuntu/OcelotApp/MAX.npy", MAX)
-    np.save("/home/ubuntu/OcelotApp/PTP.npy", PTP)
+# if CHANGE_PTP:
+#     temp = [MAX[x] - MIN[x] for x in range(len(PTP))]
+#     PTP = np.array(temp)
+#     np.save("/home/ubuntu/OcelotApp/MIN.npy", MIN)
+#     np.save("/home/ubuntu/OcelotApp/MAX.npy", MAX)
+#     np.save("/home/ubuntu/OcelotApp/PTP.npy", PTP)
 
 inptNorm = (inpt - MIN) / PTP
 
 net = NetworkReader.readFrom(
-    '/home/ubuntu/OcelotApp/NN.pybrain.net.384-100.xml'
+    '/home/ubuntu/OcelotApp/NN.pybrain.net.384-50.xml'
 )
 
 guess = net.activate(inptNorm)
